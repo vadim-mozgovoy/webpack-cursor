1482a1c9376e445d70397ca459bb7d3c537cd9d7
Vadim <60232088+vadim-mozgovoy@users.noreply.github.com>
2022-07-17 23:32:19 +0300

    add folder sounds

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '../src/styles/style-hw10.css'
 import '../src/img/dj-playing-music-at-sound-mixer-in-night-club_53876-18678.jpg'
 import '../src/modules/hw-10.js'
 import '../src/modules/hw-9.js'
+import '../src/sounds'
 import {getRandomNumber} from "./modules/hw-3";
 import {apple,orange,lemon} from "./modules/hw-1";
 import {sumProducts} from "./modules/hw-1";
